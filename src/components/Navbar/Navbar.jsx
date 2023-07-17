bdcc8a2e77d088bb1d7af6c4048646447b0e3d28
Your Name <christofaokoro@gmail.com>
2023-07-17 03:09:16 +0100

    file setup

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,39 @@
+// import "./Navbar.scss";
+// // import {BsFillTelephoneFill} from "react-icons/bs"
+
+// function Navbar() {
+//   return (
+//     <div className="Navbar">
+//       <nav>
+//         <img src="" alt="Logo" />
+//         <ul>
+//           <li>
+//             <a href="#">Home</a>
+//           </li>
+//           <li>
+//             <a href="#">Programs</a>
+//           </li>
+//           <li>
+//             <a href="#">Education</a>
+//           </li>
+//           <li>
+//             <a href="#">Alumini</a>
+//           </li>
+//           <li>
+//             <a href="#">About</a>
+//           </li>
+//         </ul>
+//         <ul className="list-2">
+//           <li>
+//             <a href="#">Login</a>
+//           </li>
+//           <li className="li2">
+//             <button> <BsFillTelephoneFill size={25}/> +234 906 8149 238</button>
+//           </li>
+//         </ul>
+//       </nav>
+//     </div>
+//   );
+// }
+
+// export default Navbar;
