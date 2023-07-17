acf9ea30e5af25534c268f681ecedd2b825fc090
Your Name <christofaokoro@gmail.com>
2023-07-17 05:48:07 +0100

    components setup

---
+++ src/pages/Degree/Degree.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../../components/Layout/Layout'
+
+function Degree() {
+  return (
+    <Layout>
+      <div></div>
+    </Layout>
+  )
+}
+
+export default Degree
