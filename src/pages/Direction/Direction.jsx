919ba59b8445a363052395b1116b13f07cc3d78c
Your Name <christofaokoro@gmail.com>
2023-07-22 10:34:21 +0100

    direction page

--- src/pages/Direction/Direction.jsx
+++ src/pages/Direction/Direction.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
-import Layout from '../../components/Layout/Layout'
+import React from "react";
+import Layout from "../../components/Layout/Layout";
+import Explore from "../../components/Explore/Explore";
+import "./Direction.scss";
 
 function Direction() {
   return (
     <Layout>
-      <div></div>
+    <div className="direction">
+        <div className="direction_header">
+        <h1>Direction</h1>
+      </div>
+
+      {/* <div className="container">
+        <button>
+          <a href="#">GET LIVE DIRECTION TO THE DEPARTMENTAL BUILDING</a>
+        </button>
+
+        <div>
+          <img src="/images/Map.jpg" alt="map" />
+        </div>
+      </div> */}
+    </div>
     </Layout>
-  )
+  );
 }
 
-export default Direction
+export default Direction;
