a3df5bbec3b8adf84d38dee263599800fff7463b
Your Name <christofaokoro@gmail.com>
2023-07-22 15:29:28 +0100

    staff page

--- src/pages/Team/Team.jsx
+++ src/pages/Team/Team.jsx
@@ -1,12 +1,146 @@
-import React from 'react'
-import Layout from '../../components/Layout/Layout'
+import React from "react";
+import Layout from "../../components/Layout/Layout";
+import "./Team.scss";
 
 function Team() {
   return (
     <Layout>
-      
+      <main className="Staff_page">
+        <div className="staff_header">
+          <h1>Staff Profile</h1>
+        </div>
+        <div className="staff_body">
+          <div className="staff_background">
+            <div>
+              <div className="container1">
+                <h1 className="headings">Head of Department</h1>
+                <div>
+                  <img
+                    className="image"
+                    src="/images/Staff - Photo.jpg"
+                    alt=""
+                  />
+                  <b className="title">
+                    Dr. Friday Onuodu<span>Head of Department</span>
+                  </b>
+                </div>
+              </div>
+              <div className="container2">
+                <h1 className="headings">Administrarive staffs</h1>
+                <div>
+                  <div>
+                    <img
+                      className="image"
+                      src="/images/Staff - Photo.jpg"
+                      alt=""
+                    />
+                    <b className="title">
+                      Dr. Friday Onuodu<span>Head of Department</span>
+                    </b>
+                  </div>
+                  <div>
+                    <img
+                      className="image"
+                      src="/images/Staff - Photo.jpg"
+                      alt=""
+                    />
+                    <b className="title">
+                      Dr. Friday Onuodu<span>Head of Department</span>
+                    </b>
+                  </div>
+                  <div>
+                    <img
+                      className="image"
+                      src="/images/Staff - Photo.jpg"
+                      alt=""
+                    />
+                    <b className="title">
+                      Dr. Friday Onuodu<span>Head of Department</span>
+                    </b>
+                  </div>
+                  <div>
+                    <img
+                      className="image"
+                      src="/images/Staff - Photo.jpg"
+                      alt=""
+                    />
+                    <b className="title">
+                      Dr. Friday Onuodu<span>Head of Department</span>
+                    </b>
+                  </div>
+                </div>
+              </div>
+              <div className="container3">
+                <h1 className="headings">Lecturers</h1>
+                <div>
+                  <div>
+                    <img
+                      className="image"
+                      src="/images/Staff - Photo.jpg"
+                      alt=""
+                    />
+                    <b className="title">
+                      Dr. Friday Onuodu<span>Head of Department</span>
+                    </b>
+                  </div>
+                  <div>
+                    <img
+                      className="image"
+                      src="/images/Staff - Photo.jpg"
+                      alt=""
+                    />
+                    <b className="title">
+                      Dr. Friday Onuodu<span>Head of Department</span>
+                    </b>
+                  </div>
+                  <div>
+                    <img
+                      className="image"
+                      src="/images/Staff - Photo.jpg"
+                      alt=""
+                    />
+                    <b className="title">
+                      Dr. Friday Onuodu<span>Head of Department</span>
+                    </b>
+                  </div>
+                  <div>
+                    <img
+                      className="image"
+                      src="/images/Staff - Photo.jpg"
+                      alt=""
+                    />
+                    <b className="title">
+                      Dr. Friday Onuodu<span>Head of Department</span>
+                    </b>
+                  </div>
+                  <div>
+                    <img
+                      className="image"
+                      src="/images/Staff - Photo.jpg"
+                      alt=""
+                    />
+                    <b className="title">
+                      Dr. Friday Onuodu<span>Head of Department</span>
+                    </b>
+                  </div>
+                  <div>
+                    <img
+                      className="image"
+                      src="/images/Staff - Photo.jpg"
+                      alt=""
+                    />
+                    <b className="title">
+                      Dr. Friday Onuodu<span>Head of Department</span>
+                    </b>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
     </Layout>
-  )
+  );
 }
 
-export default Team
+export default Team;
